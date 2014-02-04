39698cbf677d256e8b08b003f6771e14c7206220
Stephanie Stroud <stephanie.marie.stroud@gmail.com>
2014-02-03 20:06:36 -0800

    JSON Menu IDs

---
+++ json_menu_ids/json_menu_ids.py
@@ -0,0 +1,54 @@
+'''
+JSON Menu IDs
+(https://www.codeeval.com/open_challenges/102/)
+
+CHALLENGE DESCRIPTION:
+You have JSON string which describes a menu. Calculate
+the SUM of IDs of all "items" in case a "label" exists
+for an item.
+
+INPUT SAMPLE:
+Your program should accept as its first argument a path
+to a filename. Input example is the following
+
+{"menu": {"header": "menu", "items": [{"id": 27}, {"id": 0,
+"label": "Label 0"}, null, {"id": 93}, {"id": 85}, {"id": 54},
+null, {"id": 46, "label": "Label 46"}]}}
+
+{"menu": {"header": "menu", "items": [{"id": 81}]}}
+
+{"menu": {"header": "menu", "items": [{"id": 70, "label":
+"Label 70"}, {"id": 85, "label": "Label 85"}, {"id": 93,
+"label": "Label 93"}, {"id": 2}]}}
+
+All IDs are integers between 0 and 100. It can be 10 items
+maximum for a menu.
+
+OUTPUT SAMPLE:
+Print results in the following way.
+46
+0
+248
+'''
+
+from sys import argv
+import json
+import re
+
+menu_file = open(argv[1])
+menus = menu_file.readlines()
+
+
+def get_sum_of_ids(menu):
+    return sum(
+        item['id'] for item in menu['menu']['items']
+        if item and 'label' in item
+    )
+
+for menu in menus:
+    menu = re.sub(' +', ' ', menu)
+    menu = menu.strip()
+    if menu:
+        menu = json.loads(menu)
+        print get_sum_of_ids(menu)
+
