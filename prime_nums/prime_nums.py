db067d4f136f9d6ae7b5d2a7e8090abd829e6f48
Stephanie Stroud <stephanie.marie.stroud@gmail.com>
2014-02-11 21:30:25 -0800

    Print N prime nums

---
+++ prime_nums/prime_nums.py
@@ -0,0 +1,57 @@
+'''
+PRINT N PRIME NUMBERS
+Not exactly, but close to this: https://www.codeeval.com/open_challenges/46/
+
+CHALLENGE DESCRIPTION:
+Print out N prime numbers. For bonus points your solution should
+run in N*(log(N)) time or better. You may assume that N is always
+a positive integer.
+
+INPUT SAMPLE:
+10
+20
+100
+
+OUTPUT SAMPLE:
+For each line of input, print out the prime numbers less than N, in ascending
+order, comma delimited. (There should not be any spaces between the comma and
+ numbers) E.g.
+
+2,3,5,7
+2,3,5,7,11,13,17,19
+2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
+'''
+
+from itertools import imap
+from string import rstrip
+from sys import argv
+
+
+def primes(num):
+
+    list_of_primes = [2]
+    counter = 3
+
+    while len(list_of_primes) < num:
+
+        is_prime = True
+
+        for prime in list_of_primes:
+
+            if counter % prime == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            list_of_primes.append(counter)
+        counter += 1
+
+    return list_of_primes
+
+
+with open(argv[1]) as input_file:
+    numbers = filter(lambda num: num, imap(rstrip, input_file))
+
+for num in numbers:
+    num = int(num)
+    print ','.join(imap(str, primes(num)))
