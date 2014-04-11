cf37b5ce8d64ebaeeedd5e69bba8e3747553e063
Stephanie Stroud <stroud109@gmail.com>
2014-04-11 11:39:49 -0700

    Debugging issue with path reassignment

--- mazes/maze.py
+++ mazes/maze.py
@@ -1,12 +1,19 @@
 # -*- coding: latin-1 -*-
 
+from pprint import pprint
 import random
+import string
 
 EAST = 'E'
 NORTH = 'N'
+chars = iter(string.ascii_uppercase)
 
-class Path(object):
-    pass
+class Tile(object):
+    def __init__(self):
+        self.name = chars.next()
+
+    def __repr__(self):
+        return 'Tile(%s)' % self.name
 
 
 def generate_edges_and_paths(width, height):
@@ -17,7 +24,7 @@ def generate_edges_and_paths(width, height):
 
     for x in range(width):
         for y in range(height):
-            paths[(x, y)] = Path()
+            paths[(x, y)] = set([Tile()])
             if y == 0 and x > 0 and x < width:
                 edge = (x, y , NORTH)
                 edges.append(edge)
@@ -37,16 +44,14 @@ def generate_maze(width, height):
     '''
     Create a function that randomly produces mazes. This function takes the
     length and width of a grid, signified by `m` and `n`, respectively.
-
-    Tiles() = indexes == immutable set
-    Paths() = group of tiles that form a maze path
-    Edges() = walls between indvidual tiles
     '''
 
     edges, paths = generate_edges_and_paths(width, height)
 
-    print 'edges: ', edges
-    print 'paths: ', paths
+    pprint(edges)
+    pprint(paths)
+    print 'original number of edges: ', len(edges)
+    print 'original number of paths: ', len(paths)
 
     # if the grid values are the same, add the edge to `walls`
     # if the grid values are different, merge the values
@@ -55,32 +60,36 @@ def generate_maze(width, height):
     while edges:
 
         edge = edges.pop(random.randint(0, len(edges) - 1))
-
+        print '\npopped edge: ', edge
         if edge[2] == EAST:
             # compare what's above and below the edge
             a_path_coords = (edge[0], edge[1])
-            print 'EAST a_path_coords: ', a_path_coords
-            # if the edges were correctly recorded, all values here should be valid
             b_path_coords = (edge[0], (edge[1] - 1))
-            print 'EAST b_path_coords: ', b_path_coords
-            if paths[a_path_coords] is not paths[b_path_coords]:
-                paths[b_path_coords] = paths[a_path_coords]
-                print 'EAST reassigned paths[a_path_coords]: ', paths[a_path_coords]
+            print 'comparing path instances at %s and %s' % (a_path_coords, b_path_coords)
+            if not paths[a_path_coords].intersection(paths[b_path_coords]):
+                print '>> NOT EQUAL, making them equal'
+                joined_path = paths[a_path_coords].union(paths[b_path_coords])
+                paths[a_path_coords] = joined_path
+                paths[b_path_coords] = joined_path
             else:
+                print '>> EQUAL equal, saving edge to list of walls'
                 walls.append(edge)
 
         elif edge[2] == NORTH:
             # compare what's on either side of the edge
             a_path_coords = (edge[0], edge[1])
-            print 'NORTH a_path_coords: ', a_path_coords
-            # if the edges were correctly recorded, all values here should be valid
             b_path_coords = ((edge[0] - 1), edge[1])
-            print 'NORTH b_path_coords: ', b_path_coords
-            if paths[a_path_coords] is not paths[b_path_coords]:
-                paths[b_path_coords] = paths[a_path_coords]
-                print 'NORTH reassigned paths[a_path_coords]: ', paths[a_path_coords]
+            print 'comparing path instances at %s and %s' % (a_path_coords, b_path_coords)
+            if not paths[a_path_coords].intersection(paths[b_path_coords]):
+                print '>> NOT EQUAL, making them equal'
+                joined_path = paths[a_path_coords].union(paths[b_path_coords])
+                paths[a_path_coords] = joined_path
+                paths[b_path_coords] = joined_path
             else:
+                print '>> EQUAL equal, saving edge to list of walls'
                 walls.append(edge)
+
+    pprint(paths)
     return walls
 
 
